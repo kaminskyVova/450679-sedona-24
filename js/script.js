87b43d274eb50c4185df3a3ee3432b0d46d3f7bb
KaminskyVova <kamok@bk.ru>
2019-03-12 11:21:17 +0200

    ////
    1) что то не получается выровнять до конца форму поиска(криво стоят элементы)
    2)по умолчанию форма должна быть открыта(вроде так написанно в задании)
    сделал дабл клик или клик правой закрывает ее,один клик открывает
    3)svg не меняют цвет!
    4)жопа кароч

---
+++ js/script.js
@@ -0,0 +1,24 @@
+﻿
+// модальное окно
+
+var modal = document.getElementById('myModal');
+var btn = document.getElementById('open_modal');
+
+btn.onclick = function () {
+    modal.style.display = "block";
+ }
+
+ btn.ondblclick = function () {
+    modal.style.display = "none";
+ }
+
+ btn.oncontextmenu = function() {
+    modal.style.display = "none";
+    return false;
+ }
+
+ /*btn.onmousedown = function() {
+    btn.style.background = "#503e33";
+ }*/
+
+

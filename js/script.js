e1cf4c0e1ca5c63e3b7062c1d32efe6e9b060fca
KaminskyVova <kamok@bk.ru>
2019-03-24 13:41:11 +0200

    закончил

--- js/script.js
+++ js/script.js
@@ -1,25 +1,63 @@
 ﻿
+
+
 // модальное окно
 
-var modal = document.getElementById('myModal');
-var btn = document.getElementById('open_modal');
+var btn = document.querySelector('.open_modal');
+var modal = document.querySelector('.myModal');
+var date_in = modal.querySelector('#date_in');
+var date_out = modal.querySelector('#date_out');
+var search = document.querySelector('.search-button');
+
+var isStorageSupport = true;
+var storage = '';
 
 
-btn.onclick = function () {
-    modal.style.display = "block";
- }
+try {
+    storage = localStorage.getItem('date_in');
+} catch (err) {
+    isStorageSupport = false;
+}
 
- btn.ondblclick = function () {
-    modal.style.display = "none";
- }
+//modal.classList.toggle('show_modal');                   //Не работает!
 
- btn.oncontextmenu = function() {
-    modal.style.display = "none";
-    return false;
- }
+btn.addEventListener('click', function(evt) {
+    evt.preventDefault(); 
+    modal.classList.toggle('show_modal');                      
+    //modal.classList.add('show_modal'); 
+    if (storage) {
+        date_in.value = storage;
+        date_out.focus();
+    } else {                  
+        date_in.focus();
+    }
+});
 
- /*btn.onmousedown = function() {
-    btn.style.background = "#503e33";
- }*/
+search.addEventListener('click', function(evt) {      // Не отменяет действие кнопки поиска "Найти"
+    evt.preventDefault();  
+});
+
+                                         
+search.addEventListener('click', function(evt) {     //     
+    if (!date_in.value || !date_out.value) {     
+        evt.preventDefault();    
+       // modal.classList.add('modal_error');                
+        alert('Нужно заполнить форму'); 
+            
+    }
+});
+
+window.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === 27) {
+        if (modal.classList.contains('show_modal')) {
+            evt.preventDefault();
+            modal.classList.remove('show_modal');
+           // modal.classList.remove('modal_error');
+        }
+    }
+});
 
 
+        
+
+    
